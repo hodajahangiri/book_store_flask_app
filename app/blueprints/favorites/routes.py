d1e7803a49a8cb4bf77278e6dac31ef2cf5dd978
HodaJahangiri <hoda.cs87@gmail.com>
2026-02-09 10:04:56 -0800

    Add toggle_favorite route to favorites_bp to make like and unlike easier.

--- app/blueprints/favorites/routes.py
+++ app/blueprints/favorites/routes.py
@@ -54,3 +54,22 @@ def delete_favorite(favorite_id):
 	return jsonify({"message": "Book removed from favorites successfully."}), 200
 
 
+@favorites_bp.route('/<int:book_description_id>', methods=['PUT'])
+@token_required
+def toggle_favorite(book_description_id):
+	user_id = int(request.user_id)
+	user = db.session.get(Users, user_id)
+	book = db.session.get(Book_descriptions, book_description_id)
+	if not user:
+		return jsonify({"error": "User not found."}), 404
+	if not book:
+		return jsonify({"error": "Book not found."}), 404
+	existed_favorite = db.session.query(Favorites).where((Favorites.user_id == user_id) & (Favorites.book_description_id == book_description_id)).first()
+	if existed_favorite:
+		db.session.delete(existed_favorite)
+		db.session.commit()
+		return jsonify({"message": "Book removed from favorites successfully."}), 200
+	favorite = Favorites(user_id=user_id, book_description_id=book_description_id)
+	db.session.add(favorite)
+	db.session.commit()
+	return jsonify({"message": f"{book.title} added to your favorites."}), 200
